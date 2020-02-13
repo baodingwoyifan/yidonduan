59620c1ab4ab411b165c0d98b1104934a9b7af56
李皓宇 <2417806418@qq,com>
2020-02-13 10:29:03 +0800

    文章列表基础展示-瀑布流效果

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,7 +1,16 @@
 <template>
   <div class="container">
     <van-tabs v-model="activeChannelIndex">
-      <van-tab title="推荐">推荐内容展示</van-tab>
+      <van-tab title="推荐">
+         <!-- 文章列表展示区域 -->
+  <div class="scroll-wrapper">
+    <!-- 文章上拉列表 -->
+    <van-list v-model="loading" :finished="finished" finished-text="没有更多了" 
+              @load="onLoad">
+      <van-cell v-for="item in list" :key="item" :title="item"/>
+    </van-list>
+  </div>
+      </van-tab>
       <van-tab title="数据库">数据库内容展示</van-tab>
       <van-tab title="后端">后端内容展示</van-tab>
     </van-tabs>
@@ -13,8 +22,54 @@ export default {
   name: 'home-index',
   data () {
     return {
-      activeChannelIndex: 2
+      list: [],
+      loading: false,
+      finished: false,
+      activeChannelIndex: 0
+    }
+  },
+  methods: {
+    onLoad () {
+      // 异步更新数据
+      setTimeout(() => {
+        for (let i = 0; i < 10; i++) {
+          this.list.push(this.list.length + 1)
+        }
+        // 加载状态结束
+        this.loading = false
+
+        // 数据全部加载完成
+        if (this.list.length >= 40) {
+          this.finished = true
+        }
+      }, 500)
     }
   }
 }
 </script>
+<style scoped lang='less'>
+.van-tabs {
+  // 弹性布局
+  display: flex;
+  // 灵活的项目将垂直显示，正如一个列一样
+  flex-direction: column;
+  height: 100%;
+  /deep/ .van-tabs__content {
+    // flex:1;的值是1 1 0%，【父控件有剩余空间占1份放大，父控件空间不足按1缩小，自身的空间大小是0%】
+    flex: 1;
+    overflow: hidden;
+  }
+  /deep/ .van-tab__pane {
+    height: 100%;
+    // 给上拉列表设置样式
+    .scroll-wrapper {
+      height: 100%;
+      overflow-y: auto;
+    }
+  }
+  // 给频道下边沿横向设置样式
+  /deep/ .van-tabs__line {
+    background-color: #1989fa;
+  }
+}
+</style>
